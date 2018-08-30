a2d7812998922e7c428d6ce5f1e45a692ac21457
Ward Van Hooreweghe <ward.vanhooreweghe@calibrate.be>
2018-08-30 21:11:26 +0200

    stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,17 @@ import Carousel from "../components/carousel";
 
 const IndexPage = () => (
   <main>
+    <Carousel/>
     <div className={"container"}>
-      <Carousel/>
       <Title value={"Podologie"}/>
       <div className="row">
-        <div className="col-sm-4">
+        <div className="col-md-4">
           <CallToAction to={'/podologie#instrumentale-behandeling'} className="nav-link">Pedicure<br/><span>(Instrumentale behandeling)</span></CallToAction>
         </div>
-        <div className="col-sm-4">
+        <div className="col-md-4">
           <CallToAction to={'/podologie#biomechanisch-onderzoek-ganganalyse'} className="nav-link">Biomechanisch onderzoek & ganganalyse</CallToAction>
         </div>
-        <div className="col-sm-4">
+        <div className="col-md-4">
           <CallToAction to={'/podologie#zooltherapie-schoenadvies'} className="nav-link">Zooltherapie en schoenadvies</CallToAction>
         </div>
       </div>
