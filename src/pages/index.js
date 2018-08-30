0f19317bff07d1455ae8c73686b1d49877167b5e
Ward Van Hooreweghe <ward.vanhooreweghe@calibrate.be>
2018-08-30 20:41:10 +0200

    Added the carousel on the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Title from "../components/title";
 import CallToAction from "../components/cta";
+import Carousel from "../components/carousel";
 
 const IndexPage = () => (
   <main>
     <div className={"container"}>
+      <Carousel/>
       <Title value={"Podologie"}/>
-
       <div className="row">
         <div className="col-sm-4">
           <CallToAction to={'/podologie#instrumentale-behandeling'} className="nav-link">Pedicure<br/><span>(Instrumentale behandeling)</span></CallToAction>
