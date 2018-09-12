bb67d62105591515c48c73b73d2420fa22313a81
Ward Van Hooreweghe <ward.vanhooreweghe@calibrate.be>
2018-09-12 20:53:19 +0200

    target blank to keep them on our webiste

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Location = () => (
   <div>
-    <p><a href={"https://www.google.com/maps/dir/?api=1&destination=Zandstraat 245, 9170 Sint-Pauwels"}>Zandstraat 245<br/>9170 Sint-Pauwels</a></p>
+    <p><a href={"https://www.google.com/maps/dir/?api=1&destination=Zandstraat 245, 9170 Sint-Pauwels"} target={"_blank"}>Zandstraat 245<br/>9170 Sint-Pauwels</a></p>
   </div>
 );
 
