70e16c3243de1d4ddc69a4a4ebacdf42c40f2dcc
Ward Van Hooreweghe <ward.van.hooreweghe@gmail.com>
2021-12-20 18:41:05 +0100

    try this?

--- docs/path---podologie-a0e39f21c11f6a62c5ab.js
+++ docs/path---podologie-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xff1f92a3faa1],{274:function(a,f){a.exports={pathContext:{}}}});
+webpackJsonp([0xff1f92a3faa1],{258:function(a,f){a.exports={pathContext:{}}}});
 //# sourceMappingURL=path---podologie-a0e39f21c11f6a62c5ab.js.map
